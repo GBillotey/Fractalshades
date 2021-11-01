06d38660f0017ebc0c2067c7094b4daaaa5a3593
GBillotey <geoffroy.billotey@gmail.com>
2021-11-01 19:49:44 +0100

    20211101 release 2.3.3 commit - correct import error with latest Pillow

--- src/fractalshades/__init__.py
+++ src/fractalshades/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = "G. Billotey"
 __license__ = "MIT"
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 
 from .settings import get_figures, close
 from .utils import zoom_options, calc_options, interactive_options
