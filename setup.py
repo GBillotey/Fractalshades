efbdd6636879820cfcac801acc55a554cf717f7c
GBillotey <geoffroy.billotey@gmail.com>
2022-01-17 18:34:58 +0100

    Setup for WIN

--- setup.py
+++ setup.py
@@ -22,6 +22,26 @@ import gmpy2
 # https://github.com/numpy/numpy/issues/11653
 # https://stackoverflow.com/questions/52749662/using-deprecated-numpy-api
 
+CONFIG_VERSION = '"2019-07-09"'
+extra_link_args = []
+
+if sys.platform == "win32":
+    # To build for Windows:
+    # 1. Install MingW-W64-builds from https://mingw-w64.org/doku.php/download
+    #    It is important to change the default to 64-bit when installing if a
+    #    64-bit Python is installed in windows.
+    # 2. Put the bin/ folder inside x86_64-8.1.0-posix-seh-rt_v6-rev0 in your
+    #    system PATH when compiling.
+    # 3. The code below will moneky-patch distutils to work.
+    import distutils.cygwinccompiler
+    distutils.cygwinccompiler.get_msvcr = lambda: []
+    # Escaping works differently.
+    CONFIG_VERSION = '\\"2019-07-09\\"'
+    # Make sure that pthreads is linked statically, otherwise we run into problems
+    # on computers where it is not installed.
+    extra_link_args = ["-Wl,-Bstatic", "-lpthread"]
+
+
 include_dirs = (
     sys.path
     + [os.path.dirname(gmpy2.__file__)]
@@ -33,7 +53,26 @@ ext_FP = setuptools.Extension(
     [r"src/fractalshades/mpmath_utils/FP_loop.pyx"],
     include_dirs=include_dirs,
     libraries=['gmp', 'mpfr', 'mpc'],
+    define_macros=[('CONFIG_VERSION', CONFIG_VERSION)],
+    extra_link_args=extra_link_args
 )
+
+# Note : MPFR under windows
+# https://github.com/AntonKueltz/fastecdsa/issues/11
+# https://preshing.com/20141108/how-to-install-the-latest-gcc-on-windows/
+# Or: https://www.msys2.org/
+# https://www.lucaswillems.com/fr/articles/59/installer-gmp-windows
+# curl https://httpbin.org/json >> $GITHUB_ENV
+
+# curl.exe -o msys2-x86_64-latest.exe https://github.com/msys2/msys2-installer/releases/download/2021-11-30/msys2-base-x86_64-20211130.sfx.exe
+# .\msys2-x86_64-latest.exe in --confirm-command --accept-messages --root C:/msys64
+
+#  Build binaries on Windows.
+# Could not find a working solution online, but were able to make
+# this work with mingw-w64. Visual Studio will not compile QuickJS.
+# https://github.com/PetterS/quickjs/commit/67bc2428b8c0716538b4583f4f2b0a2a5a49106c
+
+
 
 setuptools.setup(
     ext_modules=cythonize(
