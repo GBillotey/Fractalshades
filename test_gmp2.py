a388987b6cb8192e10c42913471cd7df327b97e0
GBillotey <geoffroy.billotey@gmail.com>
2022-01-17 22:10:28 +0100

    Attempt to build win

--- test_gmp2.py
+++ test_gmp2.py
@@ -8,4 +8,13 @@ print("gmpy2 install DIR:\n", os.path.dirname(gmpy2.__file__))
 print("gmpy2 context:\n", ctx)
 print("2 * 1.0j =", a * 2.)
 
-# Create a file
+# Hack setup.cfg
+hack_setup_str = (
+"""[build]
+compiler=mingw32
+
+"""
+)
+
+with open("setup.cfg", "a") as setup_cfg:
+    setup_cfg.write(hack_setup_str)
