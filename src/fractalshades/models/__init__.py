89a82050a9431f204b8df26efa8b70dc06142ef7
GBillotey <geoffroy.billotey@gmail.com>
2022-02-14 20:26:41 +0100

    Shallow degree N Mandelbrot

--- src/fractalshades/models/__init__.py
+++ src/fractalshades/models/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 from .perturbation_mandelbrot import Perturbation_mandelbrot
 from .mandelbrot import Mandelbrot
+from .mandelbrot_N import Mandelbrot_N
 from .collatz import Collatz
