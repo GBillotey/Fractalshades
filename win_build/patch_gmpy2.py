62218197d3fe07a1d30c18fb7b7f1a694c7708db
Geoffroy Billotey <geoffroy.billotey@gmail.com>
2022-03-16 10:01:31 +0100

    Update patch_gmpy2.py

--- win_build/patch_gmpy2.py
+++ win_build/patch_gmpy2.py
@@ -15,7 +15,7 @@ gmpy2_dir = os.path.dirname(gmpy2.__file__)
 # Find the directory for MS Visual studio cl.exe dumpbin.exe lib.exe
 # Note : should we automate this ?
 # https://stackoverflow.com/questions/54305638/how-to-find-vswhere-exe-path
-MVS_dir = r"C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\IDE"
+MVS_dir = r"C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\VC\bin"
 
 print("\n * Checking gmpy2 install")
 ctx = gmpy2.get_context()
