2705f64c0d04c10ee101c434922674c535b5fbca
GBillotey <geoffroy.billotey@gmail.com>
2025-04-13 16:51:13 +0200

    compatibility with numpy 2.2

--- local_install.sh
+++ local_install.sh
@@ -2,6 +2,7 @@
 
 # Fractalshades installation from locally built wheel (`dist` subdirectory)
 # Note: first, run build.sh to build the wheel
-python3 -m pip install fractalshades --user --force-reinstall --no-deps --no-index --find-links dist
+source bin/activate
+python3 -m pip install fractalshades --force-reinstall --no-deps --no-index --find-links dist
 
 sleep 2
