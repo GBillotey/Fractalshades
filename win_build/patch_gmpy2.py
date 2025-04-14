ce3989266ac049fef699148062e52a1877debecb
Geoffroy Billotey <geoffroy.billotey@gmail.com>
2025-04-14 21:40:11 +0200

    Update patch_gmpy2.py

--- win_build/patch_gmpy2.py
+++ win_build/patch_gmpy2.py
@@ -41,24 +41,14 @@ print("gmpy2 install DIR:\n", gmpy2_dir)
 print("gmpy2 context:\n", ctx)
 print("2 * 1.0j =", a * 2.)
 
-# print("\n * listing files in gmpy2 dir :")
-# print(os.listdir(gmpy2_dir))
 
-# gmpy2_dir content :
-#  'gmpy2.cp38-win_amd64.pyd'
-#  'gmpy2.h',
-#  'gmpy2.pxd',
-#  'libgcc_s_seh-1.dll',
-#  'libgmp-10.dll',
-#  'libmpc-3.dll',
-#  'libmpfr-6.dll',
-#  'libwinpthread-1.dll',
-#  '__init__.pxd',
-#  '__init__.py',
-#  '__pycache__'
+#  * listing files in site-package gmpy2.libs :
+# ['gmp.lib', 'libgcc_s_seh-1.dll', 'libgmp-10.dll', 'libmpc-3.dll', 'libmpfr-6.dll', 'libwinpthread-1.dll', 'mpc.lib', 'mpfr.lib']
+
+
 
 # print("\n * Adding necessary header files to gmpy2 insatll dir")
-# # Currently we store the heeaders locally ; we could also download them
+# # Currently we store the headers locally ; we could also download them
 # # at runtime
 # for header in glob.glob("win_build/gmpy2_headers/*.h"):
 #     print("Copy header file", header, "-->", gmpy2_dir)
@@ -72,18 +62,18 @@ print("2 * 1.0j =", a * 2.)
 # print("...", os.environ["PATH"][-600:])
 
 
-# print("\n * Generate import library from the dlls")
-# # Note : need dumpbin and lib in path
-# # Note path sep under windows : '\'
-# for dll in glob.glob(gmpy2_dir + "/" + "*.dll"):
-#     print(">>> dll file:", dll)
-#     os_exc = rf"win_build\dll2lib.bat 64 {dll}"
-#     print("execute:", os_exc)
-#     os.system(os_exc)
+print("\n * Generate import library from the dlls")
+# Note : need dumpbin and lib in path
+# Note path sep under windows : '\'
+for dll in glob.glob(gmpy2_libs + "/" + "*.dll"):
+     print(">>> dll file:", dll)
+     os_exc = rf"win_build\dll2lib.bat 64 {dll}"
+     print("execute:", os_exc)
+     os.system(os_exc)
 
 # Move the created .lib files to gmpy2 install dir
-# for lib in glob.glob("*lib"):
-#    shutil.copy2(lib, gmpy2_dir)
+for lib in glob.glob("*lib"):
+    shutil.copy2(lib, gmpy2_libs)
     
-# print("executed, folder content:")
-# print(os.listdir(gmpy2_dir))
+print("executed, site-package gmpy2.libs content:")
+print(os.listdir(gmpy2_libs))
